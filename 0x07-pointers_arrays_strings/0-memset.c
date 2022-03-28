352563002e8d653da2d78871cc70cecf0e07a2dc
Ngurepm94 <99288259+Ngurepm94@users.noreply.github.com>
2022-03-28 18:35:56 +0300

    m

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,17 +1,32 @@
-#include "main.h"
+#ifndef main
 
-/**
- * _memset - fills memory with a constant byte
- * @s: memory area
- * @b: constant byte
- * @n: bytes of the memory area
- * Return: pointer to the memory area s
- */
-char *_memset(char *s, char b, unsigned int n)
-{
-	char *ptr = s;
+int _putchar(char c);
 
-	while (n--)
-		*s++ = b;
-	return (ptr);
-}
+/* 0-memset.c */
+char *_memset(char *s, char b, unsigned int n);
+
+/* 1-memcpy.c */
+char *_memcpy(char *dest, char *src, unsigned int n);
+
+/* 2-strchr.c */
+char *_strchr(char *s, char c);
+
+/* 3-strspn.c */
+unsigned int _strspn(char *s, char *accept);
+
+/* 4-strpbrk.c */
+char *_strpbrk(char *s, char *accept);
+
+/* 5-strstr.c */
+char *_strstr(char *haystack, char *needle);
+
+/* 7-print_chessboard.c */
+void print_chessboard(char (*a)[8]);
+
+/* 8-print_diagsums.c */
+void print_diagsums(int *a, int size);
+
+/* 100-set_string.c */
+void set_string(char **s, char *to);
+
+#endif
